20dd201eae933a774bcdb3f3563dd2e6a7353401
Lapo Calamandrei <calamandrei@gmail.com>
2012-02-03 17:54:45 +0100

    Widgets: converted simple scale sliders to png, added missing          dark variation assets.
             Make the rendering script generate just not existing stuff.

--- themes/Adwaita/gtk-3.0/render-assets.sh
+++ themes/Adwaita/gtk-3.0/render-assets.sh
@@ -8,7 +8,16 @@ ASSETS_DIR="assets"
 INDEX="assets.txt"
 
 for i in `cat $INDEX`
-do $INKSCAPE --export-id=$i --export-id-only --export-png=$ASSETS_DIR/$i.png $SRC_FILE; 
-[ -f $OPTIPNG ] && $OPTIPNG -o7 -quiet $ASSETS_DIR/$i.png
+do 
+if [ -f $ASSETS_DIR/$i.png ]; then
+    echo $ASSETS_DIR/$i.png exists.
+else
+    echo
+    echo Rendering $ASSETS_DIR/$i.png
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+fi
 done
 exit 0
