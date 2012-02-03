d45b0a1a7a137b3a76a44482cb9662c9658b00f1
Lapo Calamandrei <calamandrei@gmail.com>
2012-02-03 14:52:10 +0100

    Widgets: converted menuitem assets to png.
             Run optipng on all pngs and added it to the rendering
             script.

--- themes/Adwaita/gtk-3.0/render-assets.sh
+++ themes/Adwaita/gtk-3.0/render-assets.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
-FILE="assets.svg"
+
+INKSCAPE="/usr/bin/inkscape"
+OPTIPNG="/usr/bin/optipng"
+
+SRC_FILE="assets.svg"
 ASSETS_DIR="assets"
 INDEX="assets.txt"
+
 for i in `cat $INDEX`
-do inkscape --export-id=$i --export-id-only --export-png=$ASSETS_DIR/$i.png $FILE; 
+do $INKSCAPE --export-id=$i --export-id-only --export-png=$ASSETS_DIR/$i.png $SRC_FILE; 
+[ -f $OPTIPNG ] && $OPTIPNG -o7 -quiet $ASSETS_DIR/$i.png
 done
 exit 0
